79535c29959c597b2e09658c3ecfb8c76f45682e
Nahiza <nahizakhan32@gmail.com>
2026-08-14 01:33:26 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -121,9 +122,10 @@ app.post("/orders", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("---------------------------------");
-  console.log("Server running on port 5000");
-  console.log("http://localhost:5000");
+  console.log(`Server running on port ${PORT}`);
   console.log("---------------------------------");
 });
